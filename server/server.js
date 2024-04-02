5231059b3159fba547e0fbcea1ab8266a23a9082
ryzen <ryzensingh@gmail.com>
2024-04-02 21:42:13 +0530

    server PORT change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT;
 const faceapi = require('face-api.js');
 // app.use('@tensorflow/tfjs-node');
 
