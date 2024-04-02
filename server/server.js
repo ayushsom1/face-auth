56e92df07bc3a44a43bf95dc84fd9d37603e961d
ryzen <ryzensingh@gmail.com>
2024-04-02 20:19:12 +0530

    rename modelname

--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ const faceapi = require('face-api.js');
 // app.use('@tensorflow/tfjs-node');
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ryzensingh:admin123@cluster0.kzz5ihj.mongodb.net/faceAuth');
 
 async function loadModels() {
-    const model_url = '../faceSom'
+    const model_url = '../Weights'
     await faceapi.nets.faceRecognitionNet.loadFromDisk(model_url);
     await faceapi.nets.faceLandmark68Net.loadFromDisk(model_url);
     await faceapi.nets.ssdMobilenetv1.loadFromDisk(model_url);
